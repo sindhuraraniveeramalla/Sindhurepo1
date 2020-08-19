71a5c278dcac815c9b5d342e9d1d1f53533d8417
Swetha <Swetha@DESKTOP-5DBVVF0>
2020-08-19 20:11:50 +0530

    First commit

---
+++ TestHelloworld/src/Hellogit.java
@@ -0,0 +1,9 @@
+
+public class Hellogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hi, Iam creating my first project pipeline");
+	}
+
+}
